bade972eae452e71a5ad0545e264365ae1667ca5
Digital Mohalla <connect@digitalmohalla.com>
2025-04-30 04:11:42 +0530

    Add files via upload

---
+++ types/third-party-modules.d.ts
@@ -0,0 +1,7 @@
+// src/types/third-party-modules.d.ts
+
+declare module 'potrace';
+declare module 'quantize';
+declare module 'nearest-color';
+declare module 'string.prototype.replaceall';
+declare module 'get-image-colors';
